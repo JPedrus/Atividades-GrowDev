0ac1f9e5003344963c72b9520ccca81a79dc30be
João Pedro Pereira Fernandes <joaopedropf1898@gmail.com>
2024-03-18 11:46:42 -0300

    Atualização final atividade js

---
+++ Atividade-Js-Simples/Ex8/index.js
@@ -0,0 +1,10 @@
+let numero1 = prompt("Digite um numero:");
+let numero2 = prompt("Digite outro numero:");
+
+if (numero1 == numero2) {
+    alert("Os números são iguais!")
+} else if (numero1 > numero2) {
+    alert("O primeiro número é maior!")
+} else {
+    alert("O segundo número é maior!")
+}
