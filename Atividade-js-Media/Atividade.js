8d5f903c61ec5f0067db31d503c96f75c6ed45ea
João Pedro Pereira Fernandes <joaopedropf1898@gmail.com>
2024-08-14 22:02:05 -0300

    att2 14.08.2024

--- Atividade-js-Media/Atividade.js
+++ Atividade-js-Media/Atividade.js
@@ -3,8 +3,8 @@ console.log("olá mundo")
 console.log('Média')
 
 let nota1 = 90
-let nota2 = 90
-let nota3 = 75
+let nota2 = 50
+let nota3 = 35
 let peso1 = 2
 let peso2 = 3
 let peso3 = 5
@@ -12,4 +12,8 @@ let peso3 = 5
 let soma = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
 let media = soma
 
-document.write('Sua média é: ', media)
+if (media >= 70) {
+    document.write('Aluno aprovado sua média é: ', media)
+} else {
+    document.write('Aluno reprovado sua média é: ', media)
+}
---
+++ Atividade-js-Media/Ex2.js
@@ -0,0 +1,7 @@
+let idade = 26
+let idadeMeses = (idade * 12)
+let idadeDias = (idade * 365)
+
+document.write('Sua idade em anos é: ', idade, '<br><br>') 
+document.write('Sua idade em meses é: ', idadeMeses, '<br><br>')
+document.write('Sua idade em dias é: ', idadeDias, '<br><br>')
