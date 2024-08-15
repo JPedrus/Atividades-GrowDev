9f7c6c4ed2a5173d4674c8066b1b6a29653c6539
João Pedro Pereira Fernandes <joaopedropf1898@gmail.com>
2024-08-14 21:32:36 -0300

    att 14.08.2024

--- Atividade-js-Media/Atividade.js
+++ Atividade-js-Media/Atividade.js
@@ -3,13 +3,13 @@ console.log("olá mundo")
 console.log('Média')
 
 let nota1 = 90
-let nota2 = 88
+let nota2 = 90
 let nota3 = 75
 let peso1 = 2
 let peso2 = 3
 let peso3 = 5
 
-let soma = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) / (peso1 + peso2 + peso3)
+let soma = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
 let media = soma
 
-console.log(media)
+document.write('Sua média é: ', media)
